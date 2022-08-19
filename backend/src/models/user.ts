4c6dfc269a09c46e84abc711986392ba0a44487b
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-08-19 12:53:30 -0300

    backend: create DB connection and user model

---
+++ backend/src/models/user.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose"
+import bcrypt from "bcryptjs"
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+UserSchema.pre("save", async function(next) {
+  const hash = await bcrypt.hash(this.password, 10)
+  this.password = hash
+
+  next()
+})
+
+const User = model("User", UserSchema)
+
+export default User
