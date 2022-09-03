dfe008736f41ccc5f27768d4cfeed40a66edccfc
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-09-03 20:53:25 -0300

    backend: Add get profile service

--- backend/src/controllers/profile.ts
+++ backend/src/controllers/profile.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express"
 
+import User from "../models/user"
+
 export async function getProfile(req: Request, res: Response) {
-  res.status(200).json({
-    ok: true,
-    userId: (req as Request & { userId: string }).userId
-  })
+  try {
+    const user = await User.findById((req as Request & { userId: string }).userId).select("-_id -__v")
+
+    return res.status(200).json(user)
+  } catch (error) {
+    console.error(error)
+    return res.status(400).json({ error: "Error on getting profile" })
+  }
 }
